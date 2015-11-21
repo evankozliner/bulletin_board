d6fb0ca72706dc77aa882237d642d3334b0cb1fa
evankozliner <evankozliner@gmail.com>
2015-11-21 17:55:39 -0500

    added group post. took a bit

--- multi_board_server.rb
+++ multi_board_server.rb
@@ -15,8 +15,27 @@ class Group
 	def get_messages(number = 2)
 	end
 
-	# Sends a message to all the clients in the group
-	def send_message(message)
+	# Sends a message to all the clients in the group in
+	# the format: "Message ID, Sender, Post Date, Subject"
+	def propogate_message(message)
+		puts "Propogating message"
+		message_string = message.id.to_s + ", " +
+			message.client.name.to_s + ", " +
+			message.date.to_s + ", " +
+			message.subject.to_s
+		puts "Sending out " + message_string + "..."
+		@clients.each do |client|
+			puts "puts client name: " + client.name.to_s
+			puts "message client: " + message.client.name.to_s
+			unless client.name.to_s == message.client.name.to_s
+				begin
+					client.send(message.client.name.to_s, [message_string])
+				rescue => e
+					puts e
+				end
+			end
+		end
+		puts "propogated message"
 	end
 
 	# Returns a list of usernames of clients in the group
@@ -27,10 +46,45 @@ class Group
 		end
 		return names
 	end
+
+	# Returns a list of the message ids in a group
+	def get_message_ids
+		ids = []
+		@messages.each do |message|
+			ids << message.id
+		end
+		puts ids.to_s
+		return ids
+	end
+
+	# Adds a message to the group by providing the message an id, 
+	# adding it to the messages attribute and sending it to the clients
+	def post(client, contents, subject)
+		puts "generating message"
+		id = if get_message_ids().empty? then 0 else get_message_ids().max + 1 end
+		new_message = Message.new(id,
+															subject,
+															contents,
+															client)
+		@messages << new_message
+		propogate_message(new_message)
+	end
+end
+
+class Message
+	attr_accessor :id, :subject, :contents, :client, :date
+
+	def initialize(id, subject, contents, client)
+		@id = id
+		@subject = subject
+		@contents = contents
+		@date = Time.now
+		@client = client
+	end
 end
 
 class Client
-	attr_accessor :name, :session
+	attr_accessor :name, :session, :client_name
 	
 	# Sends a JSON response to the client containing a list of strings 
 	# Appends the sender in brackets to each line
@@ -44,10 +98,6 @@ class Client
 		}.to_json
 		@session.puts json_message
 	end
-end
-
-class Message
-	attr_accessor :id, :subject, :contents
 end
 
 # Singleton for the bulletin board server
@@ -146,6 +196,19 @@ class Server
 		client.send("Server", resp)
 	end
 
+	# Adds a message to a particular group
+	def handle_post(client, message, subject, group_name)
+		group = get_group_by_name(group_name)
+		resp = ["Group " + group_name +
+					 	" doesn't exist or you do not belong to that group."]
+		if group and group.get_usernames.include? client.name.to_s 
+			puts "posting to " + group_name
+			group.post(client, message, subject)
+		else
+			client.send("Server", resp)
+		end
+	end
+
 	# Fetches a group by its (unique) name, returns false if the group
 	# doesn't exist
 	def get_group_by_name(name)
@@ -158,7 +221,7 @@ class Server
 	end
 	
 	# Takes a specific word as a command and calls a response function based on
-	# that command
+	# that command. Retreives values from json hash for the handling functions.
 	def act_on_command(first_word, client_hash, client)
 		puts "handling " + first_word
 		case first_word
@@ -167,6 +230,10 @@ class Server
 		when 'groups'
 			handle_groups(client)
 		when 'grouppost'
+			handle_post(client, 
+									client_hash["message"], 
+									client_hash["subject"],
+									client_hash["groupId"])
 		when 'groupusers'
 			handle_users(client, client_hash["groupId"])
 		when 'groupleave'
